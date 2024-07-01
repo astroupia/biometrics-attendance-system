28b4069e1f151e0e6c204557b8357c25c1c79182
Nahom Tegaude <nahomteguade@gmail.com>
2024-07-01 18:59:48 +0300

    Email OTP Verification Fix

--- mail.js
+++ mail.js
@@ -1,47 +1,37 @@
-class MailSender{
-    nodemailer;
-    gmail;
-    mailfrom;
-    tomail;
-    sub;
-    message;
-    
-    constructor(to, sub, msg){
-        this.mailfrom = 'captureit@gmail.com';
-        this.tomail = to;
-        this.sub = sub;
-        this.message = msg;
-        this.nodemailer = require('nodemailer');
-        this.gmail = this.nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-              user: 'acaptureit@gmail.com',
-              pass: 'Captureit#123'
-            }
-          });
-        console.log('Mail obj created')
-    }
+class MailSender {
+  constructor(to, sub, msg) {
+    this.mailfrom = "captureit@gmail.com";
+    this.tomail = to;
+    this.sub = sub;
+    this.message = msg;
+    this.nodemailer = require("nodemailer");
+    this.gmail = this.nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "bisratdagne99@gmail.com",
+        pass: "jodx umhp zqwk rjuh",
+      },
+    });
+    console.log("Mail obj created");
+  }
 
-    send(){
-      console.log("sending mail")
-      var mailOptions = {
-        from: this.mailfrom,
-        to: this.tomail,
-        subject: this.sub,
-        html: this.message,
-        //html: "<b>From Capture It</b>", // html body
-      };
-      
-      this.gmail.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
-  };
+  send() {
+    console.log("sending mail");
+    var mailOptions = {
+      from: this.mailfrom,
+      to: this.tomail,
+      subject: this.sub,
+      html: this.message,
+    };
 
+    this.gmail.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  }
 }
 
 module.exports = MailSender;
-
