d3c49894a321fc52f4841094fe99302a9e7c095c
Nahom Tegaude <nahomteguade@gmail.com>
2024-08-08 14:39:53 +0300

    Session Update

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
-var createError = require("http-errors");
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var expressHbs = require("express-handlebars");
-// var mongoose = require("mongoose");
-var session = require("express-session");
-var passport = require("./config/passport");
-var flash = require("connect-flash");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+const session = require("express-session");
+const passport = require("./config/passport");
+const flash = require("connect-flash");
 const validator = require("express-validator");
-var MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo")(session);
 const Handlebars = require("handlebars");
 const {
   allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
-
-var indexRouter = require("./routes/index");
-var userRouter = require("./routes/user");
-var classrouter = require("./routes/classroom");
-
-var app = express();
-
-// Connect to MongoDB
+const createError = require("http-errors");
 const mongoose = require("mongoose");
 
-// Use the service name 'mongo' as the host
-const mongoUri =
-  process.env.MONGO_URI || "mongodb://mongo:27017/attendance_portal";
+// Routes
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const classRouter = require("./routes/classroom");
 
-mongoose
-  .connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+// Initialize Express
+const app = express();
+
+// Middleware setup
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(validator());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Session setup
+app.use(
+  session({
+    secret: "mysupersecret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    cookie: { maxAge: 100 * 60 * 1000 },
   })
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.error("MongoDB connection error:", err));
+);
 
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// Set up Handlebars view engine
 const hbs = expressHbs.create({
   defaultLayout: "layout",
   extname: ".hbs",
@@ -82,47 +94,39 @@ const hbs = expressHbs.create({
   },
 });
 
-// View engine setup
 app.engine(".hbs", hbs.engine);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(validator());
-app.use(cookieParser());
-app.use(
-  session({
-    secret: "mysupersecret",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    cookie: { maxAge: 100 * 60 * 1000 },
+// Connect to MongoDB
+const mongoUri =
+  process.env.MONGO_URI || "mongodb://mongo:27017/attendance_portal";
+mongoose
+  .connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-);
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(path.join(__dirname, "public")));
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.use(function (req, res, next) {
+// Middleware for local variables
+app.use((req, res, next) => {
   res.locals.login = req.isAuthenticated();
   res.locals.session = req.session;
   next();
 });
 
-app.use("/classroom", classrouter);
-app.use("/user", userRouter);
+// Routes setup
 app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.use("/classroom", classRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// Error handling
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err.stack); // Log error stack
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -130,8 +134,9 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
+// Start the server
 const port = 3000;
-app.listen(port, "0.0.0.0", function (err) {
+app.listen(port, "0.0.0.0", (err) => {
   if (err) {
     console.error("Error starting server:", err);
   } else {
--- config/passport.js
+++ config/passport.js
@@ -1,55 +1,53 @@
-var passport = require("passport");
-var User = require("../models/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
 const MailSender = require("../mail");
 
-var LocalStrategy = require("passport-local").Strategy;
+console.log("Passport.js file is loaded.");
 
-passport.serializeUser(function (user, done) {
+// Serialize user
+passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
+// Deserialize user
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
     done(err, user);
   });
 });
 
+// Email validation
 function validateEmail(email) {
-  var re =
-    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(email)) {
-    if (
-      email.indexOf("@gmail.com", email.length - "@gmail.com".length) !== -1
-    ) {
-      return 1;
-    }
-  }
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email) && email.endsWith("@gmail.com");
 }
 
+// Password validation
 function validatePassword(password) {
-  var p = password;
-  errors = [];
-  if (p.length < 6) {
+  const errors = [];
+  if (password.length < 6) {
     errors.push("Your password must be at least 6 characters.");
   }
-  if (p.length > 15) {
-    errors.push("Your password must be atmost 15 characters.");
+  if (password.length > 15) {
+    errors.push("Your password must be at most 15 characters.");
   }
-  if (p.search(/[A-Z]/i) < 0) {
-    errors.push("Your password must contain at least one upercase letter.");
+  if (!/[A-Z]/.test(password)) {
+    errors.push("Your password must contain at least one uppercase letter.");
   }
-  if (p.search(/[a-z]/i) < 0) {
+  if (!/[a-z]/.test(password)) {
     errors.push("Your password must contain at least one lowercase letter.");
   }
-  if (p.search(/[0-9]/) < 0) {
+  if (!/[0-9]/.test(password)) {
     errors.push("Your password must contain at least one digit.");
   }
-  if (p.search(/[!@#$%^&*]/) < 0) {
+  if (!/[!@#$%^&*]/.test(password)) {
     errors.push("Your password must contain at least one special character.");
   }
   return errors;
 }
 
+// Local registration strategy
 passport.use(
   "local-register",
   new LocalStrategy(
@@ -58,128 +56,86 @@ passport.use(
       passwordField: "password",
       passReqToCallback: true,
     },
-    function (req, email, password, done) {
-      var name = req.body.name;
-      var classs = req.body.class;
-      var rollnumber = req.body.rollnumber;
-      var teacher = req.body.teacher;
-      var student = req.body.student;
-      var messages = [];
-      var year = req.body.year;
+    (req, email, password, done) => {
+      const {
+        name,
+        class: classs,
+        rollnumber,
+        teacher,
+        student,
+        year,
+      } = req.body;
+      let messages = [];
+
       req.checkBody("email", "Invalid email").notEmpty().isEmail();
       req.checkBody("password", "Invalid password").notEmpty();
-      var errors = req.validationErrors();
+      const errors = req.validationErrors();
+
       if (errors) {
-        errors.forEach(function (error) {
-          messages.push(error.msg);
-        });
+        errors.forEach((error) => messages.push(error.msg));
         return done(null, false, req.flash("error", messages));
-      } else if (!validateEmail(email)) {
+      }
+
+      if (!validateEmail(email)) {
         messages.push("Email Domain: @gmail.com required");
         return done(null, false, req.flash("error", messages));
-      } else if (validatePassword(password).length != 0) {
-        messages = validatePassword(password);
-        return done(null, false, req.flash("error", messages));
-      } else if (!teacher && !student) {
+      }
+
+      const passwordErrors = validatePassword(password);
+      if (passwordErrors.length > 0) {
+        return done(null, false, req.flash("error", passwordErrors));
+      }
+
+      if (!teacher && !student) {
         messages.push("Please check the tickbox");
         return done(null, false, req.flash("error", messages));
-      } else if (year == "Year") {
+      }
+
+      if (year === "Year") {
         messages.push("Please select a year");
         return done(null, false, req.flash("error", messages));
-      } else if (classs == "Class") {
+      }
+
+      if (classs === "Class") {
         messages.push("Please select a class");
         return done(null, false, req.flash("error", messages));
-      } else {
-        if (rollnumber) {
-          User.findOne({ rollnumber: rollnumber }, function (err, user) {
-            if (err) {
-              return done(err);
-            }
-            if (user) {
-              console.log(rollnumber);
-              return done(null, false, {
-                message: "Roll number already in use.",
-              });
-            } else {
-              User.findOne({ email: email }, function (err, user) {
-                if (err) {
-                  return done(err);
-                }
-                if (user) {
-                  return done(null, false, {
-                    message: "Email already in use.",
-                  });
-                } else {
-                  var newUser = new User();
-                  newUser.name = name;
-                  newUser.email = email;
-                  newUser.password = newUser.encryptPassword(password);
-                  newUser.class = classs;
-                  newUser.rollnumber = rollnumber;
-                  if (teacher == "0") {
-                    newUser.who = teacher;
-                  } else if (student) {
-                    newUser.who = student;
-                    if (!year) {
-                      messages.push("Please enter all details");
-                      return done(null, false, req.flash("error", messages));
-                    } else {
-                      newUser.year = year;
-                    }
-                  } else {
-                    newUser.who = "";
-                  }
-                  newUser.save(function (err, result) {
-                    if (err) {
-                      throw err;
-                    }
-                    return done(null, newUser);
-                  });
-                }
-              });
-            }
-          });
-        } else {
-          User.findOne({ email: email }, function (err, user) {
-            if (err) {
-              return done(err);
-            }
-            if (user) {
-              return done(null, false, { message: "Email already in use." });
-            } else {
-              var newUser = new User();
-              newUser.name = name;
-              newUser.email = email;
-              newUser.password = newUser.encryptPassword(password);
-              newUser.class = classs;
-              newUser.rollnumber = rollnumber;
-              if (teacher == "0") {
-                newUser.who = teacher;
-              } else if (student) {
-                newUser.who = student;
-                if (!year) {
-                  messages.push("Please enter all details");
-                  return done(null, false, req.flash("error", messages));
-                } else {
-                  newUser.year = year;
-                }
-              } else {
-                newUser.who = "";
-              }
-              newUser.save(function (err, result) {
-                if (err) {
-                  throw err;
-                }
-                return done(null, newUser);
-              });
-            }
-          });
-        }
       }
+
+      const checkUserExists = (criteria, errorMsg) =>
+        User.findOne(criteria).then((user) => {
+          if (user) {
+            return Promise.reject(errorMsg);
+          }
+        });
+
+      // Check if roll number or email already exists
+      const checkPromises = [
+        rollnumber &&
+          checkUserExists({ rollnumber }, "Roll number already in use."),
+        checkUserExists({ email }, "Email already in use."),
+      ].filter(Boolean);
+
+      Promise.all(checkPromises)
+        .then(() => {
+          const newUser = new User({
+            name,
+            email,
+            password: new User().encryptPassword(password),
+            class: classs,
+            rollnumber,
+            who: teacher || student || "",
+            year: student ? year : undefined,
+          });
+
+          return newUser.save();
+        })
+        .then((newUser) => done(null, newUser))
+        .catch((err) => done(err, false, req.flash("error", [err.message])));
     }
   )
 );
 
+// Local login strategy
 passport.use(
   "local-login",
   new LocalStrategy(
@@ -188,34 +144,29 @@ passport.use(
       passwordField: "password",
       passReqToCallback: true,
     },
-    function (req, email, password, done) {
-      //Validation Errors
+    (req, email, password, done) => {
       req.checkBody("email", "Invalid Email").notEmpty().isEmail();
       req.checkBody("password", "Invalid password").notEmpty();
-      var errors = req.validationErrors();
+      const errors = req.validationErrors();
+
       if (errors) {
-        var messages = [];
-        errors.forEach(function (error) {
-          messages.push(error.msg);
-        });
+        const messages = errors.map((error) => error.msg);
         return done(null, false, req.flash("error", messages));
       }
 
-      //Find user by email
-      User.findOne({ email: email }, function (err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          return done(null, false, { message: "User not found." });
-        }
-
-        if (!user.validPassword(password)) {
-          return done(null, false, { message: "Invalid Password" });
-        }
-
-        return done(null, user);
-      });
+      User.findOne({ email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, { message: "User not found." });
+          }
+          if (!user.validPassword(password)) {
+            return done(null, false, { message: "Invalid Password" });
+          }
+          return done(null, user);
+        })
+        .catch((err) => done(err));
     }
   )
 );
+
+module.exports = passport;
